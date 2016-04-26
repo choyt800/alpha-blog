099b768addc64bd710f46313d46b22f1bd34b6e4
Chris <choyt800@gmail.com>
2016-04-26 22:03:18 +0000

    added articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   
  root 'pages#home'
  
- get 'pages/home', to: 'pages#home'
-
- get 'pages/about', to: 'pages#about'
+ get 'about', to: 'pages#about'
  
+ resources :articles
+
 end
