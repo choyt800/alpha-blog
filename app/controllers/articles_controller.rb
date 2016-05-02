f17634cf8748f1036caf89d52891558f7d7f7208
Chris <choyt800@gmail.com>
2016-05-02 23:52:43 +0000

    added show, index, destroy actions in articles_controller and corresponding views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,16 +4,47 @@ class ArticlesController < ApplicationController
        @article = Article.new
    end
    
+   def edit 
+       @article = Article.find(params[:id])
+   end
+   
    def create
-    #   render plain: params[:article].inspect
        @article = Article.new(article_params)
-       @article.save
-       redirect_to articles_show(@article)
+       if @article.save
+           flash[:notice] = "Article was successfully created"
+           redirect_to article_path(@article)
+       else
+           render 'new'
+       end
+       
+   end
+   
+   def update 
+       @article = Article.find(params[:id])
+       if @article.update(article_params)
+           flash[:notice] = "Article was successfully updated"
+           redirect_to article_path(@article)
+       else 
+           render 'edit'
+       end
    end
    
    def show
+       @article = Article.find(params[:id])
+   end
+
+   def index
+       @articles = Article.all
    end
    
+   def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      
+      redirect_to articles_path
+      flash[:notice] = "article # " + @article.id.to_s + " was successfully destroyed"
+   end
+
    private
     def article_params
         params.require(:article).permit(:title, :description)
